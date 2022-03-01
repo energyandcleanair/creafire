ed83f7052eafcfb0962df259b81208d9f31e7f18
Hubert Thieriot <hubert.thieriot@gmail.com>
2022-03-01 12:32:51 +0000

    -better handling weather cache

--- R/weather.R
+++ R/weather.R
@@ -33,32 +33,35 @@ update_weather <- function(weather,
     dplyr::mutate(geometry=suppressWarnings(sf::st_centroid(geometry))) %>%
     sf::st_as_sf(sf_column_name="geometry", crs = 4326)
 
-
-  # old_trajs <- readRDS(file_trajs)
-
-  print("Getting new weather data")
-  new_weather <- creadeweather::collect_weather(
-    meas_missing,
-    years=years,
-    years_force_refresh=years,
-    n_per_station=4,
-    add_pbl=any(grepl("pbl", names(weather))),
-    add_sunshine=F,
-    add_fire=any(grepl("fire", names(weather))),
-    fire_source=fire_source,
-    fire_mode="trajectory",
-    fire_duration_hour=duration_hour,
-    fire_buffer_km=buffer_km,
-    trajs_height=height
-  )
-
-  # print("Updating trajectories file")
-  # bind_rows(old_trajs,
-  #           readRDS(file_trajs) %>% select_at(names(old_trajs))) %>%
-  #   saveRDS(file_trajs)
-
-  print("Combining weather")
-  updated_weather <- bind_rows(weather,
-                               new_weather)
+  
+  if(nrow(meas_missing)>0){
+    print("Getting new weather data")
+    new_weather <- creadeweather::collect_weather(
+      meas_missing,
+      years=years,
+      years_force_refresh=years,
+      n_per_station=4,
+      add_pbl=any(grepl("pbl", names(weather))),
+      add_sunshine=F,
+      add_fire=any(grepl("fire", names(weather))),
+      fire_source=fire_source,
+      fire_mode="trajectory",
+      fire_duration_hour=duration_hour,
+      fire_buffer_km=buffer_km,
+      trajs_height=height
+    )
+    
+    # print("Updating trajectories file")
+    # bind_rows(old_trajs,
+    #           readRDS(file_trajs) %>% select_at(names(old_trajs))) %>%
+    #   saveRDS(file_trajs)
+    
+    print("Combining weather")
+    updated_weather <- bind_rows(weather,
+                                 new_weather)
+  }else{
+    updated_weather <- weather
+  }
+  
   return(updated_weather)
 }
